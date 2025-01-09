f9ee22d23904334513b2d17c7b2c9058b7995517
Amaresh Dhani <adhani@adobe.com>
2025-01-09 12:33:20 +0530

    placeholder access

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,8 +11,12 @@ import {
   loadSection,
   loadSections,
   loadCSS,
+  fetchPlaceholders
 } from './aem.js';
 
+// fetch placeholders from the 'en' folder
+const placeholders = await fetchPlaceholders('en');
+console.log(placeholders)
 /**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
