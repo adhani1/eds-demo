118098abf7398b54a8886e2f230451ea893c67b3
Amaresh Dhani <adhani@adobe.com>
2025-01-13 12:43:49 +0530

    files added

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,18 +1,27 @@
-export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
+export default async function  decorate(block) {
+
+ const response=await fetch("http://localhost:3000/customblocks.json")
+  const content =await response.json()
+  content.data.forEach(element => {
+    const dynamicdiv=document.createElement('div')
+    dynamicdiv.textContent=`${element.Name} : ${element.Content}`
+    block.appendChild(dynamicdiv)
+  });
+
+  // const cols = [...block.firstElementChild.children];
+  // block.classList.add(`columns-${cols.length}-cols`);
 
   // setup image columns
-  [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
-      }
-    });
-  });
+  // [...block.children].forEach((row) => {
+  //   [...row.children].forEach((col) => {
+  //     const pic = col.querySelector('picture');
+  //     if (pic) {
+  //       const picWrapper = pic.closest('div');
+  //       if (picWrapper && picWrapper.children.length === 1) {
+  //         // picture is only content in column
+  //         picWrapper.classList.add('columns-img-col');
+  //       }
+  //     }
+  //   });
+  // });
 }
